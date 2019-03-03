16bc267b838438663c3535684446a5e1c068497a
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2019-03-03 00:43:33 -0500

    all items have images

--- src/itemPopulation/leftTreeHelper.java
+++ src/itemPopulation/leftTreeHelper.java
@@ -7,12 +7,33 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
  
-public class leftTreeHelper 
+public class leftTreeHelper
 {
-	private final Node rootIcon2 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon2.png")));
-	private final Node rootIcon3 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon3.png")));
-	private final Node rootIcon4 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon4.png")));
-	private final Node rootIcon5 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon5.png")));
+	private Node rootIcon2 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon2.png")));
+	private Node rootIcon3 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon3.png")));
+	private Node rootIcon4 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon4.png")));
+	private Node rootIcon5 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon5.png")));
+	private Node adminEventsIcon =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/icon2a.png")));
+	
+	private Node iconFolder =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconFolder.png")));
+	private Node iconFolder2 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconFolder.png")));
+	private Node iconFolder3 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconFolder.png")));
+
+	private Node iconPlain =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconPlain.png")));
+	private Node iconPlain2 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconPlain.png")));
+	
+	private Node iconWarn =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn2 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn3 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn4 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn5 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn6 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn7 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn8 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn9 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+	private Node iconWarn10 =  new ImageView(new Image(getClass().getResourceAsStream("/imageAssets/iconWarn.png")));
+
+	
 	
     public leftTreeHelper()
     {
@@ -46,7 +67,7 @@ public class leftTreeHelper
     private ArrayList<TreeItem> getCustViews()
     {
         ArrayList<TreeItem> custView = new ArrayList<TreeItem>();
-        TreeItem adminEvents = new TreeItem("Administrative Events");
+        TreeItem adminEvents = new TreeItem("Administrative Events", adminEventsIcon);
          
         custView.add(adminEvents);      
         return custView;        
@@ -57,11 +78,11 @@ public class leftTreeHelper
     {
         ArrayList<TreeItem> winLogs = new ArrayList<TreeItem>();
          
-        TreeItem app = new TreeItem("Application");
-        TreeItem sec = new TreeItem("Security");
-        TreeItem setup = new TreeItem("Setup");
-        TreeItem system = new TreeItem("System");
-        TreeItem forwardedEvents = new TreeItem("Forwarded Events");
+        TreeItem app = new TreeItem("Application", iconWarn);
+        TreeItem sec = new TreeItem("Security", iconWarn2);
+        TreeItem setup = new TreeItem("Setup", iconPlain);
+        TreeItem system = new TreeItem("System", iconWarn3);
+        TreeItem forwardedEvents = new TreeItem("Forwarded Events", iconPlain2);
          
         winLogs.add(app);
         winLogs.add(sec);
@@ -77,21 +98,25 @@ public class leftTreeHelper
     {
         ArrayList<TreeItem> apps = new ArrayList<TreeItem>();
          
-        TreeItem aesm = new TreeItem("AESMService");
-        TreeItem hardware = new TreeItem("Hardware Events");
-        TreeItem audio = new TreeItem("IntelAudioServiceLog");
-        TreeItem ie = new TreeItem("Internet Explorer");
-        TreeItem keyManage = new TreeItem("KeyManagement Service");
-        TreeItem office = new TreeItem("Microsoft Office Alerts");
-        TreeItem pre = new TreeItem("PreEmptive");
-        TreeItem powersh = new TreeItem("Windows Powershell");
+        TreeItem aesm = new TreeItem("AESMService", iconFolder);
+        TreeItem hardware = new TreeItem("Hardware Events", iconWarn4);
+        TreeItem audio = new TreeItem("IntelAudioServiceLog", iconWarn5);
+        TreeItem ie = new TreeItem("Internet Explorer", iconWarn6);
+        TreeItem keyManage = new TreeItem("KeyManagement Service", iconWarn7);
+        TreeItem microsoft = new TreeItem("Microsoft", iconFolder2);
+        TreeItem office = new TreeItem("Microsoft Office Alerts", iconWarn8);
+        TreeItem openSSH = new TreeItem("OpenSSH", iconFolder3);
+        TreeItem pre = new TreeItem("PreEmptive", iconWarn9);
+        TreeItem powersh = new TreeItem("Windows Powershell", iconWarn10);
         
         apps.add(aesm);
         apps.add(hardware);
         apps.add(audio);
         apps.add(ie);
         apps.add(keyManage);
+        apps.add(microsoft);
         apps.add(office);
+        apps.add(openSSH);
         apps.add(pre);
         apps.add(powersh);
          
