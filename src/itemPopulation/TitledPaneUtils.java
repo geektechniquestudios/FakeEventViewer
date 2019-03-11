f5b202fb68147535a6bb8c8581f56973e4758c14
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2019-03-11 05:20:20 -0400

    styled scrollbars appropriately

--- src/itemPopulation/TitledPaneUtils.java
+++ src/itemPopulation/TitledPaneUtils.java
@@ -26,6 +26,26 @@ public final class TitledPaneUtils {
             return leftInset - textLayout;
         }, title.paddingProperty(), text.layoutXProperty()));
     }
+    
+    public static void putArrowOnRightLess(TitledPane pane) {
+        Region title = (Region) pane.lookup(".title");
+        Region arrow = (Region) title.lookup(".arrow-button");
+        Node text = title.lookup(".text");
+
+        arrow.translateXProperty().bind(Bindings.createDoubleBinding(() -> {
+            double titleWidth = title.getWidth();
+            double arrowWidth = arrow.getWidth();
+            double rightInset = title.getPadding().getRight();
+            double leftInset = title.getPadding().getLeft();
+            return titleWidth - arrowWidth - rightInset - leftInset - 15;
+        }, title.widthProperty(), title.paddingProperty(), arrow.widthProperty()));
+
+        text.translateXProperty().bind(Bindings.createDoubleBinding(() -> {
+            double leftInset = title.getPadding().getLeft();
+            double textLayout = text.getLayoutX();
+            return leftInset - textLayout;
+        }, title.paddingProperty(), text.layoutXProperty()));
+    }
 
     // prevent instantiation
     private TitledPaneUtils() {}
