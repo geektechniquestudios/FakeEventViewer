156860f6500ef8e0ad851cd179e74558df924e9b
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2019-03-17 23:44:51 -0400

    basis for image replacement laid out

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
@@ -19,11 +21,17 @@ public class Main extends Application
 			primaryStage.getIcons().add(new Image("/imageAssets/icon.png"));
 			//primaryStage.setMaximized(true); // does EV start full screen normally?
 			
+			
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("EventViewer.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			new FXMLLoader(getClass().getResource("HangmanFile.fxml"));
+			
+			EventViewerController controller = new EventViewerController();
+			controller.setRoot(root);
 		} 
 		catch(Exception e) 
 		{
